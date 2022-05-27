f2879e7e623a163ab1df22d3315c3649dd397bd7
kvaghasiya31 <106371557+kvaghasiya31@users.noreply.github.com>
2022-05-27 16:18:00 +0530

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,29 @@
+
+public class PascalExercise {
+
+	public static void main (String args[])
+	{
+		
+				
+		for(int i = 5 ; i > 0 ; i--)
+		{
+			for (int j = i ; j < 5 ; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("*");
+			System.out.println();
+		}
+		 for (int j = 4 ; j > 0 ; j--) { System.out.print("*"); }
+		 System.out.println();
+		 
+		 for (int k = 3 ; k > 0 ; k--) { System.out.print("*"); }
+		 System.out.println();
+		  
+		 for (int l = 2 ; l > 0 ; l--) { System.out.print("*"); }
+		 System.out.println();
+		  
+		 for (int m = 1 ; m > 0 ; m--) { System.out.print("*"); }
+		
+	}
+}
